61b9c6568b93e366292877e26e999b7325674007
Pritam Sharma <pritamsharma3648@gmail.com>
2024-01-07 23:05:16 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:howdy/Pages/loginpage.dart';
+import 'package:howdy/theme/light_theme.dart';
+
+void main() {
+  runApp(
+    const Howdy(),
+  );
+}
+
+class Howdy extends StatelessWidget {
+  const Howdy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Howdy Chat App',
+      theme: lightmode,
+      home: const LoginPage(),
+    );
+  }
+}
